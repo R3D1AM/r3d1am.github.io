2acf06576b91591648c125ea68a381258d1f2450
M4R <45825973+R3D1AM@users.noreply.github.com>
2023-09-07 23:41:56 +0100

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,103 @@
+var mastadon = 'https://infosec.exchange/@R3D1AM';
+var linkedin = 'https://www.linkedin.com/in/mdrurb/';
+var github = 'https://github.com/R3D1AM';
+var research = 'https://www.researchgate.net/profile/Maider-D-U';
+var post1 =
+	'https://github.com/R3D1AM/Blog/blob/main/Data%20Hound%20-%20Explained.md';
+
+whois = [
+	'<br>',
+	'Cypherpunk.',
+	'Malware Alchemist.',
+	'Cryptography Ninja.',
+	'Lockpick Extraordinaire.',
+
+	'<br>',
+	'Did you just take both pills?',
+	'<br>',
+];
+
+blog = [
+	'<br>',
+	'Blog Post       <a href="' +
+		post1 +
+		'" target="_blank">Data Hound - Explained' +
+		'</a>',
+	'<br>',
+];
+
+social = [
+	'<br>',
+	'mastadon       <a href="' +
+		mastadon +
+		'" target="_blank">mastadon/@R3D1AM' +
+		'</a>',
+	'linkedin       <a href="' +
+		linkedin +
+		'" target="_blank">linkedin/@mdrurb' +
+		'</a>',
+	'github         <a href="' +
+		github +
+		'" target="_blank">github/@R3D1AM' +
+		'</a>',
+	'<br>',
+	'Not really a social media person, I barely use any of these.',
+	'<br>',
+];
+
+projects = [
+	'<br>',
+	'Alot of projects are offline or confidential',
+	'but you can find some stuff on my <a href="' +
+		github +
+		'" target="_blank">github' +
+		'</a>',
+	'and some other stuff on my <a href="' +
+		research +
+		'" target="_blank">research page' +
+		'</a>',
+	'<br>',
+];
+
+help = [
+	'<br>',
+	'<span class="command">help</span>           Commands Menu',
+	'<span class="command">whois</span>          Who is R3D1AM',
+	'<span class="command">blog</span>           Read stuff',
+	'<span class="command">projects</span>       View projects',
+	'<span class="command">social</span>         Social media links',
+	'<span class="command">history</span>        View command history',
+	'<span class="command">clear</span>          Clear terminal',
+	'<span class="command">home</span>           Back to Home',
+	'<br>',
+];
+
+home = [
+	'<span class="index">R3D1AM | Terminal</span>',
+
+	'	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+	'	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡾⠿⢿⡀⠀⠀⠀⠀⣠⣶⣿⣷⠀⠀⠀⠀',
+	'	⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣦⣴⣿⡋⠀⠀⠈⢳⡄⠀⢠⣾⣿⠁⠈⣿⡆⠀⠀⠀',
+	'	⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠿⠛⠉⠉⠁⠀⠀⠀⠹⡄⣿⣿⣿⠀⠀⢹⡇⠀⠀⠀',
+	'	⠀⠀⠀⠀⠀⣠⣾⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⣰⣏⢻⣿⣿⡆⠀⠸⣿⠀⠀⠀',
+	'	⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣆⠹⣿⣷⠀⢘⣿⠀⠀⠀',
+	'	⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⠋⠉⠛⠂⠹⠿⣲⣿⣿⣧⠀⠀',
+	'	⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣿⣷⣾⣿⡇⢀⠀⣼⣿⣿⣿⣧⠀',
+	'	⠰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡘⢿⣿⣿⣿⠀',
+	'	⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣷⡈⠿⢿⣿⡆',
+	'	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠁⢙⠛⣿⣿⣿⣿⡟⠀⡿⠀⠀⢀⣿⡇',
+	'	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣶⣤⣉⣛⠻⠇⢠⣿⣾⣿⡄⢻⡇',
+	'	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣦⣤⣾⣿⣿⣿⣿⣆⠁',
+	'	',
+	'         ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄',
+	'         ██░▄▄▀█░▄▄░█░▄▄▀█▀░██░▄▄▀█░▄▀▄░██',
+	'         ██░▀▀▄███▄▀█░██░██░██░▀▀░█░█░█░██',
+	'         ██░██░█░▀▀░█░▀▀░█▀░▀█░██░█░███░██',
+	'         ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀',
+	'© 2023. All rights reserved.',
+	'',
+	'################################################',
+	'#<span class="color2">Welcome to my corner of the internet.</span>         #',
+	'#<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>#',
+	'################################################',
+];
